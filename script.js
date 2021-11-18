0d73830c1b9e91c8f98e0b510770237bdb6a7d16
Noah2211 <noah.decroix3@gmail.com>
2021-11-18 10:56:20 +0100

    exercice done

--- script.js
+++ script.js
@@ -4,8 +4,9 @@ let button = document.getElementById('switch');
  button.addEventListener("click", function (event) {
      let input1 = document.getElementById("input1");
      let input2 = document.getElementById("input2");
-     let label1 = document.getElementById("label1");
-     let label2 = document.getElementById('label2');
-     label1.innerText = input2.value;
-     label2.innerText = input1.value;
+     let valeurInput1 = input1.value;
+     input1.value = input2.value;
+     input2.value = valeurInput1;
+
+
  })
